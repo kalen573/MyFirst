c1dc368d28647dcf75f0c05b6d1c66c9180ff45c
kalen <kalen573@gmail.com>
2023-06-26 11:58:42 +0900

    francクラスを作成

---
+++ franc.py
@@ -0,0 +1,16 @@
+class Franc:
+    @property
+    def amount(self):
+        return self.__amount
+
+    def __init__(self, amount):
+        self.__amount = amount
+
+    def times(self, multiplier: int):
+        #return self.amount * multiplier
+        return Franc(self.__amount * multiplier)
+    
+    def __eq__(self, money):
+        franc = money 
+        return self.__amount == franc.amount
+#　第5章
