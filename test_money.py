4cbb088c00dd520dca36fd2fe61fab2687144310
kalen <kalen573@gmail.com>
2023-06-26 11:45:08 +0900

    francのテストを追加

--- test_money.py
+++ test_money.py
@@ -1,7 +1,7 @@
 from dollar import Dollar
 
 class TestMoney:
-    def test_multiplication(self):
+    def test_DollarMultiplication(self):
         five = Dollar(5)
         assert Dollar(10) == five.times(2)
         assert Dollar(15) == five.times(3)
@@ -9,3 +9,8 @@ class TestMoney:
     def test_equals(self):
         assert Dollar(5) == Dollar(5)
         assert not Dollar(5) == Dollar(6)
+
+    def test_FrancMultiplication(self):
+        five = Franc(5)
+        assert Franc(10) == five.times(2)
+        assert Franc(15) == five.times(3)
