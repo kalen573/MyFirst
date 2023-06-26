ddcca8a1c153f639954c5f2870050b59a578d5a4
kalen <kalen573@gmail.com>
2023-06-26 11:25:17 +0900

    テストの変更

--- test_money.py
+++ test_money.py
@@ -3,12 +3,9 @@ from dollar import Dollar
 class TestMoney:
     def test_multiplication(self):
         five = Dollar(5)
-        product = five.times(2)
-        assert 10 == product.amount
+        assert Dollar(10) == five.times(2)
+        assert Dollar(15) == five.times(3)
 
-        product = five.times(3)
-        assert 15 == product.amount
-
-    def test_equal(self):
+    def test_equals(self):
         assert Dollar(5) == Dollar(5)
         assert not Dollar(5) == Dollar(6)
