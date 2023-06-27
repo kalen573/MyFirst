9ed266b3775ba4340a2fe388b9f5c70272e303ff
kalen <kalen573@gmail.com>
2023-06-27 11:16:20 +0900

    francにMoneyを継承。eqを移動削除

--- franc.py
+++ franc.py
@@ -1,16 +1,9 @@
-class Franc:
-    @property
-    def amount(self):
-        return self.__amount
+from money import Money
 
+class Franc(Money):
     def __init__(self, amount):
         self.__amount = amount
 
     def times(self, multiplier: int):
         #return self.amount * multiplier
         return Franc(self.__amount * multiplier)
-    
-    def __eq__(self, money):
-        franc = money 
-        return self.__amount == franc.amount
-#　第5章完了
