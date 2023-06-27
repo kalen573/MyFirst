f58aedb5b8037990caf7bb022dd95c5a4c28bfcd
kalen <kalen573@gmail.com>
2023-06-27 16:35:41 +0900

    二つの通貨クラスをMoneyに移動、暮らすメソッドを追加

--- money.py
+++ money.py
@@ -9,4 +9,29 @@ class Money:
     def __eq__(self, object):
         money = object
         return (self.__amount == money.amount and self.__class__.__name__ == object.__class__.__name__)
+    
+    @classmethod
+    def dollar(cls, amount):
+        return Dollar(amount)
+    
+    @classmethod
+    def franc(cls, amount):
+        return Franc(amount)
+
    
+class Dollar(Money):
+    def __init__(self, amount):
+        super(Dollar, self).__init__(amount)
+        self.__amount = amount
+
+    def times(self, multiplier: int):
+        return Dollar(self.amount * multiplier)
+    
+class Franc(Money):
+    def __init__(self, amount):
+        super(Franc, self).__init__(amount)
+        self.__amount = amount
+
+    def times(self, multiplier: int):
+        #return self.amount * multiplier
+        return Franc(self.amount * multiplier)
--- test_money.py
+++ test_money.py
@@ -4,18 +4,18 @@ from money import Money
 
 class TestMoney:
     def test_DollarMultiplication(self):
-        five = Dollar(5)
-        assert Dollar(10) == five.times(2)
-        assert Dollar(15) == five.times(3)
+        five = Money.dollar(5)
+        assert Money.dollar(10) == five.times(2)
+        assert Money.dollar(15) == five.times(3)
 
     def test_equality(self):
-        assert Dollar(5) == Dollar(5)
-        assert not Dollar(5) == Dollar(6)
-        assert Franc(5) == Franc(5)
-        assert not Franc(5) == Franc(6)
-        assert not Franc(5) == Dollar(5)
+        assert Money.dollar(5) == Money.dollar(5)
+        assert not Money.dollar(5) == Money.dollar(6)
+        assert Money.franc(5) == Money.franc(5)
+        assert not Money.franc(5) == Money.franc(6)
+        assert not Money.franc(5) == Money.dollar(5)
 
     def test_FrancMultiplication(self):
-        five = Franc(5)
-        assert Franc(10) == five.times(2)
-        assert Franc(15) == five.times(3)
+        five = Money.franc(5)
+        assert Money.franc(10) == five.times(2)
+        assert Money.franc(15) == five.times(3)
