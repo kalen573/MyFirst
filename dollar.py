6c2a5926c49574a33a0becc37ff5010b9bf3f89d
kalen <kalen573@gmail.com>
2023-06-27 16:17:55 +0900

    第6章完了

--- dollar.py
+++ dollar.py
@@ -1,18 +1,9 @@
 from money import Money
 
 class Dollar(Money):
-
-    # @property
-    # def amount(self):
-    #     return self.__amount
-
     def __init__(self, amount):
         super(Dollar, self).__init__(amount)
         self.__amount = amount
 
     def times(self, multiplier: int):
         return Dollar(self.amount * multiplier)
-    
-    # def __eq__(self, object):
-    #     money = object
-    #     return self.__amount == money.amount
--- franc.py
+++ franc.py
@@ -1,10 +1,6 @@
 from money import Money
 
 class Franc(Money):
-    # @property
-    # def amount(self):
-    #     return self.__amount
-
     def __init__(self, amount):
         super(Franc, self).__init__(amount)
         self.__amount = amount
@@ -12,7 +8,3 @@ class Franc(Money):
     def times(self, multiplier: int):
         #return self.amount * multiplier
         return Franc(self.amount * multiplier)
-    
-    # def __eq__(self, object):
-    #     money = object
-    #     return self.__amount == money.amount
