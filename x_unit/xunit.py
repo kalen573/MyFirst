97c639d537d3c4902a6dd0a3ec3087bbe74771c8
kalen <kalen573@gmail.com>
2023-07-20 16:53:40 +0900

    tearDownメソッドを追加

--- x_unit/xunit.py
+++ x_unit/xunit.py
@@ -5,22 +5,27 @@ class TestCase:
     def setUp(self):
         pass
 
+    def tearDown(self):
+        pass
+
     def run(self):
         self.setUp()
         method = getattr(self, self.name)
         method()
+        self.tearDown()
 
 class WasRun(TestCase):
     def setUp(self):
         self.log = "setUp "    
     def testMethod(self):
         self.log = self.log + "testMethod "
+    def tearDown(self):
+        self.log = self.log + "tearDown"
 
 class TestCaseTest(TestCase):
     def testTemplateMethod(self):
         test = WasRun("testMethod")
         test.run()
-        assert test.log == "setUp testMethod "
+        assert test.log == "setUp testMethod tearDown"
 
-TestCaseTest("testRunning").run()
 TestCaseTest("testTemplateMethod").run()
