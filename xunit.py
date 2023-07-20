86f51ee3f0e2635dfd2f4fb3c6f0fd18757a7cd7
kalen <kalen573@gmail.com>
2023-07-20 11:50:09 +0900

    runメソッド修正　親クラスTestCaseの設定と継承

--- xunit.py
+++ xunit.py
@@ -1,8 +1,16 @@
-class WasRun:
+class TestCase:
+    def __init__(self, name):
+        self.name = name
+
+
+
+class WasRun(TestCase):
     def __init__(self, name):
         self.wasRun = None
+        super().__init__(name)
     def run(self):
-        self.testMethod()
+        method = getattr(self, self.name)
+        method()
     def testMethod(self):
         self.wasRun = 1
 
